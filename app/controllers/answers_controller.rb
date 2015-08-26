04e6ede3db0cc380f4a4464d772782c4dde4a6c9
Antonio Martel <antoniomartel@gmail.com>
2015-08-26 08:12:42 +0100

    Changes to stop visualizing answers from this app

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,6 @@
 # This file is app/controllers/answers_controller.rb
 class AnswersController < ApplicationController
   def index
-    @answers = Answer.all
+    # @answers = Answer.all
   end
 end
